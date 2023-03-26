d678e2c46ae68471bee439bcdb68a57a45f9cdb9
Tim Post <tim@echoreply.io>
2023-03-26 18:08:50 -0400

    Component just for shorts

--- src/components/YouTubePlayer/index.js
+++ src/components/YouTubePlayer/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import styles from './index.module.css';
 
+export function YouTubeShortPlayer({id}) {
+    var url = `https://www.youtube-nocookie.com/embed/${id}`;
+    return (
+        <div className={styles.shortEmbed}>
+            <iframe  
+                src={url}
+                title="YouTube video player" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowFullScreen
+                ></iframe>
+        </div>
+    );
+}
+
 export default function YouTubePlayer({id}) {
     var url = `https://www.youtube-nocookie.com/embed/${id}`;
     return (
