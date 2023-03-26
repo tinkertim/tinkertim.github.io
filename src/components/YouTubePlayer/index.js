32478712eed3c62a1ddb47e021995326f3bef60e
Tim Post <tim@echoreply.io>
2023-03-26 14:26:19 -0400

    Video Player Component

---
+++ src/components/YouTubePlayer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './index.module.css';
+
+export default function YouTubePlayer({id}) {
+    var url = `https://www.youtube-nocookie.com/embed/${id}`;
+    return (
+        <div className={styles.videoEmbed}>
+            <iframe  
+                src={url}
+                title="YouTube video player" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowFullScreen
+                ></iframe>
+        </div>
+    );
+}
