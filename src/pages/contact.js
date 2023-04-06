e8da642d877ecf98045d07d918248f2945bd9a90
Tim Post <tim@echoreply.io>
2023-04-06 11:20:01 -0400

    Contact page updates

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,13 +11,17 @@ function ContactPageContent() {
       <div className="container">
         <h1 className="margin-top--lg">How To Get In Touch:</h1>
         <p className="margin-top--md">
-          The best way to reach me is via e-mail; I'm <code>tinkertim</code>
-          {" "}
-          at the free mail service that Google runs.
+          Please note, I no longer work for Stack Overflow, and I can't provide support for
+          account issues that you might be having with them. Additionally, I can't help with 
+          policy type questions, as I'm very out-of-the-loop. If there's something I can do 
+          as an ordinary "citizen" of SO or the Stack Exchange network, feel free to drop in!
+        </p>
+        <p className="margin-top--md">
+          The best way to reach me is via e-mail; I'm <code>tinkertim@gmail.com</code>
         </p>
         <p>
-          You can also reach out to me on Twitter. My DMs on Twitter are open,
-          but it may take me a few days to see your message request.
+          You can also reach out to me on Twitter or LinkedIn - see the links in the footer. My DMs on 
+          Twitter are open, but it may take me a few days to see your message request. 
         </p>
       </div>
     </section>
