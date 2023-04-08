fada5c8f5302254b22a95f8c19d9b358990cc7e7
Tim Post <tim@echoreply.io>
2023-04-08 11:47:12 -0400

    About page anecdote

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,25 +18,23 @@ function AboutPageContent() {
               </strong>
             </p>
             <p className="padding-top--md">
+              Curious about the book? It's not real, but it might help you! If you ever have to build a
+              "professional" site and feel like one more picture of your face online will
+              break the universe: paint yourself as a children's book cover. You're welcome.
+            </p>
+            <p className="padding-top--md">
               I got my start in tech back in the 1990s, during the first dot-com
               bubble. I'd been writing code pretty regularly since learning C
               when I was about 15 after learning BASIC and simple assembly years
               before on a TRS-80 and C-64.
             </p>
             <p className="padding-top--md">
-              I spent the first half of my programming career strictly in the
+              The first half of my programming career was spent pretty strictly in the
               UNIX world. I worked on file systems, multiple kernels, shells,
               hypervisors and what led to be known as IAAS / PAAS. It was my
               journey in the hosting industry that ultimately landed me in The
               Philippines, where I settled for nearly 15 years and started a
               family.
-            </p>
-            <p className="padding-top--md">
-              I became known as someone who could pull over-budget death-marches
-              back into the black. This was hugely challenging and rewarding,
-              but too crushing to continue. I decided to explore the human
-              aspects of software development and joined the Stack Overflow
-              Community Growth Team.
             </p>
           </div>
           <div class="col col--6">
@@ -49,7 +47,14 @@ function AboutPageContent() {
           </div>
         </div>
         <div class="row padding-horiz--md">
-          <p className="margin-top--lg">
+        <p className="padding-top--md">
+              I became known as someone who could pull over-budget death-marches
+              back into the black. This was hugely challenging and rewarding,
+              but too crushing to continue. I decided to explore the human
+              aspects of software development and joined the Stack Overflow
+              Community Growth Team.
+            </p>
+          <p className="margin-top--md">
             In 2013, "DevRel" really wasn't a thing yet. While DevRel wasn't
             exactly <em>invented</em>
             at Stack Overflow, we were one of the first teams put in a PR role
@@ -61,7 +66,7 @@ function AboutPageContent() {
             A few years later, I was running the team as the Director of
             Community Strategy. I had also just been diagnosed with stage IV
             lung cancer that had spread, and spread rapidly, through my brain. I
-            was lucky enough to achive 100% remission, which is soon entering
+            was lucky enough to achieve 100% remission, which is soon entering
             its seventh year. But, this came at a cost, both physical and
             financial, and left me with multiple disabilities. I write about
             this experience probably more than I should, but going through it
