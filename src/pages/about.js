5cbbf0d326bb5626bf5c4cc98a3cbf744ff54a7e
Tim Post <tim@echoreply.io>
2023-04-18 10:09:45 -0400

    FINALLY figured out what was bothering me about this page. Fix columns.

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,12 +18,6 @@ function AboutPageContent() {
               </strong>
             </p>
             <p className="padding-top--md">
-              Curious about the book? It's not real, but it might help you! If
-              you ever have to build a "professional" site and feel like one
-              more picture of your face online will break the universe: paint
-              yourself as a children's book cover. You're welcome.
-            </p>
-            <p className="padding-top--md">
               I got my start in tech back in the 1990s, during the first dot-com
               bubble. I'd been writing code pretty regularly since learning C
               when I was about 15 after learning BASIC and simple assembly years
@@ -37,6 +31,13 @@ function AboutPageContent() {
               in The Philippines, where I settled for nearly 15 years and
               started a family.
             </p>
+            <p className="padding-top--md">
+            I became known as someone who could pull over-budget death-marches
+            back into the black. This was hugely challenging and rewarding, but
+            too crushing to continue. I decided to explore the human aspects of
+            software development and joined the Stack Overflow Community Growth
+            Team.
+          </p>
           </div>
           <div class="col col--5">
             <img
@@ -47,23 +48,17 @@ function AboutPageContent() {
             />
           </div>
         </div>
-        <div class="row padding-horiz--md">
-          <p className="padding-top--md">
-            I became known as someone who could pull over-budget death-marches
-            back into the black. This was hugely challenging and rewarding, but
-            too crushing to continue. I decided to explore the human aspects of
-            software development and joined the Stack Overflow Community Growth
-            Team.
-          </p>
-          <p className="margin-top--md">
+        <div class="row margin-top--md">
+          <div className="col col--12">
+            <p className="padding-top--md">
             In 2013, "DevRel" really wasn't a thing yet. While DevRel wasn't
             exactly <em>invented</em>
             at Stack Overflow, we were one of the first teams put in a PR role
             and comprised solely of former developers specifically so we'd have
             that perspective. We may not have been the first, but we were
             definitely some of the originals.
-          </p>
-          <p>
+            </p>
+            <p className="padding-top--md">
             A few years later, I was running the team as the Director of
             Community Strategy. I had also just been diagnosed with stage IV
             lung cancer that had spread, and spread rapidly, through my brain. I
@@ -72,13 +67,20 @@ function AboutPageContent() {
             financial, and left me with multiple disabilities. I write about
             this experience probably more than I should, but going through it
             really turned me into an advocate on many fronts.
-          </p>
-          <p className="margin-bottom--xl">
+            </p>
+            <p className="padding-top--md">
             Now, I do mostly contract work as my physical health allows, while
             also focusing time on putting my experiences to work in advocacy for
             not just disabled folks re-gaining their skills in tech, but also
             those fighting their own cancer battles.
-          </p>
+            </p>
+            <p className="padding-top--md margin-bottom--xl">
+              Curious about the book? It's not real, but it might help you! If
+              you ever have to build a "professional" site and feel like one
+              more picture of your face online will break the universe: paint
+              yourself as a children's book cover. You're welcome.
+            </p>
+          </div>
         </div>
       </div>
     </section>
